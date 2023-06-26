8cc6b346915ca811f2447740b1a0d9bc3dd9d68d
Pietromuresu <muresupietro66@gmail.com>
2023-06-26 17:08:26 +0200

    First Commit initialize vue + sass + Fake Table

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <h1>Vite Boolfolio </h1>
+
+  <div class="container">
+
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Technologies</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+</template>
+
+<style lang="scss">
+
+h1{
+  text-align: center;
+  margin-top: 20px;
+}
+
+.container{
+  table{
+    width: 90%;
+    background: white;
+    margin: 0 auto;
+
+    td{
+      border: 1px solid black;
+    }
+  }
+
+}
+
+</style>  
