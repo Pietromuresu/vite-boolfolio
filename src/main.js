937b4d120e1d5612ee0ab95d187278bcc5ed3eb3
Pietromuresu <muresupietro66@gmail.com>
2023-06-27 18:54:38 +0200

    Create margin - padding utilities

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { router } from './router'
 
 import './scss/style.scss'
+import './scss/utils.scss'
 import App from './App.vue'
 
 createApp(App).use(router).mount('#app')
